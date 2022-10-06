732a698b925dfa579d0cba630e7ba7f8be3e9c99
NiicoAdasme <nicolas.wladimir@gmail.com>
2022-10-06 09:51:36 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import axios from 'axios';
+// require('dotenv').config()
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+function App() {
+
+  const apiKey = import.meta.env.VITE_REACT_APP_API_KEY;
+
+  const src = `https://www.googleapis.com/geolocation/v1/geolocate?key=${apiKey}`;
+
+  const [coords, setCoords] = useState({
+    location: {
+      lat: 0,
+      lng: 0
+    },
+    accurancy: 0
+  });
+
+  useEffect(() => {
+    
+    axios.post(src)
+    .then(function (response) {
+      // handle success
+      // console.log(response.data);
+      const { location, accurancy } = response.data;
+      // console.log(location);
+      if (location) {
+        const { lat, lng } = location;
+
+        setCoords({
+          location: {
+            lat,
+            lng
+          },
+          accurancy
+        })
+      }
+    })
+    .catch(function (error) {
+      // handle error
+      throw new error;
+    })
+  
+    
+  }, [])
+  
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Geolocalizador</h1>
+
+
+      <p>Latitude: {coords?.location?.lat}</p>
+      <p>Longitude: {coords?.location?.lng}</p>
+      <p>Accurrancy: {coords?.accurancy}</p>
+
+
+
+
+    </div>
+  )
+}
+
+export default App
