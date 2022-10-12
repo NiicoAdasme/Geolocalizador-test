0cf6206ba05d47e79f7974925e5f57b53b7caedd
NiicoAdasme <nicolas.wladimir@gmail.com>
2022-10-12 16:52:02 -0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,51 +2,55 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import axios from 'axios';
+import GoogleMapReact from 'google-map-react';
+
 
 
 function App() {
 
-  // const apiKey = import.meta.env.VITE_REACT_APP_API_KEY;
+  const apiKey = import.meta.env.VITE_REACT_APP_API_KEY;
 
-  // const src = `https://www.googleapis.com/geolocation/v1/geolocate?key=${apiKey}`;
+  const src = `https://www.googleapis.com/geolocation/v1/geolocate?key=${apiKey}`;
 
-  // const [coords, setCoords] = useState({
-  //   location: {
-  //     lat: 0,
-  //     lng: 0
-  //   },
-  //   accurancy: 0
-  // });
-
-  // useEffect(() => {
-
-  //   axios.post(src)
-  //   .then(function (response) {
-  //     // handle success
-  //     // console.log(response.data);
-  //     const { location, accurancy } = response.data;
-  //     // console.log(location);
-  //     if (location) {
-  //       const { lat, lng } = location;
-
-  //       setCoords({
-  //         location: {
-  //           lat,
-  //           lng
-  //         },
-  //         accurancy
-  //       })
-  //     }
-  //   })
-  //   .catch(function (error) {
-  //     // handle error
-  //     throw new error;
-  //   })
+  const [coords, setCoords] = useState({
+    location: {
+      lat: 0,
+      lng: 0
+    },
+    accurancy: 0
+  });
 
 
-  // }, [])
-  
-  const [coords, setCoords] = useState({latitude: '', longitude: ''})
+  useEffect(() => {
+
+    axios.post(src)
+      .then(function (response) {
+        // handle success
+        // console.log(response.data);
+        const { location, accurancy } = response.data;
+        // console.log(location);
+        if (location) {
+          const { lat, lng } = location;
+
+          setCoords({
+            location: {
+              lat,
+              lng
+            },
+            accurancy
+          })
+        }
+      })
+      .catch(function (error) {
+        // handle error
+        throw new error;
+      })
+
+
+  }, [])
+
+  // html geo api code
+  const [coord, setCoord] = useState({ latitude: '', longitude: '' })
 
   function getLocation() {
     if (navigator.geolocation) {
@@ -57,17 +61,28 @@ function App() {
   }
 
   function showPosition(position) {
-    // x.innerHTML = "Latitude: " + position.coords.latitude +
-    //   "<br>Longitude: " + position.coords.longitude;
-    const latitude  = position.coords.latitude;
+    const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
-    setCoords({
+    setCoord({
       latitude,
       longitude
     })
     console.log(`Latitude: ${latitude} | Longitude: ${longitude}`);
 
   }
+  //    end html geo api code
+
+  const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+  const defaultProps = {
+    center: {
+      lat: 0,
+      lng: 0
+    },
+    zoom: 4
+  };
+
+
 
   return (
     <div className="App">
@@ -79,18 +94,23 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Geolocalizador</h1>
+      <h1>Google Maps Geolocation API</h1>
 
 
-      {/* <p>Latitude: {coords?.location?.lat}</p>
+      <p>Latitude: {coords?.location?.lat}</p>
       <p>Longitude: {coords?.location?.lng}</p>
-      <p>Accurrancy: {coords?.accurancy}</p> */}
+      <p>Accurrancy: {coords?.accurancy}</p>
+
+
+      <h1>HTML Geolocation API</h1>
 
       <button
         onClick={getLocation}
       >Get Coords</button>
 
-      <p>Latitude: {coords.latitude} </p>  <p>Longitude: {coords.longitude} </p>
+      <p>Latitude: {coord.latitude} </p>  <p>Longitude: {coord.longitude} </p>
+
+
 
     </div>
   )
