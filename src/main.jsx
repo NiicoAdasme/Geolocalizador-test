6bbcc7289c9ff63fefbdeae5dc5b200c4638c5e1
NiicoAdasme <nicolas.wladimir@gmail.com>
2022-10-26 13:04:17 -0300

    se agregaron configuraciones para PWA

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,18 @@ import App from './components/App'
 
 import './assets/index.css'
 
+import { registerSW } from 'virtual:pwa-register'
+
+const intervalMS = 60 * 60 * 1000 // * check the service worker every hour.
+
+const updateSW = registerSW({
+  onRegistered(r) {
+    r && setInterval(() => {
+      r.update()
+    }, intervalMS)
+  }
+})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,55 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', // 'autoUpdate'
+      injectRegister: 'auto', // default: 'auto',
+      strategies: 'injectManifest',
+      injectManifest: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+      devOptions: {
+        enabled: true,
+        type: 'module'
+      },
+      manifest: {
+        name: 'Geolocalizador',
+        short_name: 'Geolocalizador',
+        description: 'Aplicacion de Geolocalizacion',
+        theme_color: '#00000',
+        icons: [
+          {
+            src: 'camanchaca300x204.jpg',
+            sizes: '192x192',
+            type: 'image/jpg'
+          },
+          {
+            src: 'camanchaca512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: 'camanchaca400.jpeg',
+            sizes: '400x400',
+            type: 'image/jpeg'
+          },
+          {
+            src: 'camanchaca680.png',
+            sizes: '680x680',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        cleanupOutdatedCaches: true,
+        sourcemap: true
+      }
+    })
+  ]
 })
